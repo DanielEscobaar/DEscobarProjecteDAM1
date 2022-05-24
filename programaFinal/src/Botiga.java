8e1af7b80b47a3e961e2e643522cffc61fd0ab81
DanielEscobaar <descobar@inspalamos.cat>
2022-05-24 18:31:56 +0200

    Projecte
    Aqui esta el projecte

---
+++ programaFinal/src/Botiga.java
@@ -0,0 +1,30 @@
+
+public class Botiga {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		
+		try 
+		{
+			
+		}
+		catch(Exception e) 
+		{
+			System.out.println("Error " + e);
+		}
+	}
+	
+	public oClient ferRegister() 
+	{
+		return null;
+	}
+	public oClient ferLogIn() 
+	{
+		return null;
+	}
+	public void menuNoClient() 
+	{
+		
+	}
+
+}
